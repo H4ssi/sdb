a321560c770021e0b744bb9e81f65ea81a90d293
Florian Hassanen <florian.hassanen@gmail.com>
2016-02-05 13:53:11 +0100

    fix: shutdown server after press of <Enter>

--- src/at/floating_integer/sdb/server/Server.java
+++ src/at/floating_integer/sdb/server/Server.java
@@ -7,36 +7,48 @@ import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Server {
 	private static final Logger L = Logger.getLogger(Server.class.getName());
 
+	private final AsynchronousChannelGroup group = AsynchronousChannelGroup //
+			.withFixedThreadPool(1, Executors.defaultThreadFactory());
+
+	private final AsynchronousServerSocketChannel serverSocket;
+
 	public Server(int port) throws IOException {
-		AsynchronousServerSocketChannel serverSocket = AsynchronousServerSocketChannel
-				.open(AsynchronousChannelGroup.withFixedThreadPool(1, Executors.defaultThreadFactory()));
+		serverSocket = AsynchronousServerSocketChannel.open(group);
 
 		serverSocket.bind(new InetSocketAddress(port));
 
-		accept(serverSocket);
+		accept();
 
 		L.info("Server started listening on port " + port);
 	}
 
-	public void accept(final AsynchronousServerSocketChannel serverSocket) {
+	private void accept() {
 		serverSocket.accept(null, new CompletionHandler<AsynchronousSocketChannel, Void>() {
 			@Override
 			public void completed(AsynchronousSocketChannel result, Void attachment) {
-				accept(serverSocket);
+				accept();
 				new Client(new ClientConnection(result));
 			}
 
 			@Override
 			public void failed(Throwable exc, Void attachment) {
 				// TODO Auto-generated method stub
-
 			}
 		});
 	}
 
+	public void shutdown() {
+		try {
+			group.shutdownNow();
+			// TODO maybe be more graceful
+		} catch (IOException e) {
+			L.log(Level.WARNING, "Error when shutting down server", e);
+		}
+	}
 }
